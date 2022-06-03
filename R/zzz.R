1956a2dcb7c4dd40a607c2ae27149d892f91034e
Indra Parta (Health) <indra.parta@health.vic.gov.au>
2022-06-03 11:00:29 +1000

    update bold file path

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
 
   showtext::font_add("VIC",
            regular = file.path(system.file("data", package = 'maecharts'),"VIC-Regular.ttf"),
-           bold  = file.path(system.file("data", package = 'maecharts'),"VIC-bold.ttf"))
+           bold  = file.path(system.file("data", package = 'maecharts'),"VIC-Bold.ttf"))
 
   showtext::showtext_auto()
 
